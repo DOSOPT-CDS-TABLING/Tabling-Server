ee729eb7d2683386eb40a732303476cb0679cf4b
ddongseop <dlehdtjq00o@gmail.com>
2023-11-21 00:07:29 +0900

    docs: #3 SuccessType 오타 수정

--- src/main/java/org/sopt/tablingServer/common/exception/model/SuccessType.java
+++ src/main/java/org/sopt/tablingServer/common/exception/model/SuccessType.java
@@ -14,7 +14,7 @@ public enum SuccessType {
      */
     PROCESS_SUCCESS(HttpStatus.OK, "OK"),
     GET_SHOP_LIST_BY_AVERAGE_WAITING_SUCCESS(HttpStatus.OK, "일 평균 대기인원을 기준으로 서울 남부 인기 매장 리스트 반환이 완료되었습니다."),
-    GET_SHOP_DETAIL_SUCCESS(HttpStatus.OK, "매장 상세 정보 반완이 완료되었습니다."),
+    GET_SHOP_DETAIL_SUCCESS(HttpStatus.OK, "매장 상세 정보 반환이 완료되었습니다."),
 
     /**
      * 201 CREATED
