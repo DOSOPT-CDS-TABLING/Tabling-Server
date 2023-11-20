bab0ce5dab6f209a09cc7b1f156a716fe510ad8e
transfer_kk <kseysh123@naver.com>
2023-11-21 01:19:18 +0900

    feat: #4 OrderController로 주문 목록 출력하는 로직의 path 구현

--- src/main/java/org/sopt/tablingServer/order/presentation/OrderController.java
+++ src/main/java/org/sopt/tablingServer/order/presentation/OrderController.java
@@ -1,4 +1,24 @@
-package org.sopt.tablingServer.order;
+package org.sopt.tablingServer.order.presentation;
 
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+import org.sopt.tablingServer.common.dto.ApiResponse;
+import org.sopt.tablingServer.order.application.OrderService;
+import org.sopt.tablingServer.order.dto.response.OrderResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.sopt.tablingServer.common.exception.model.SuccessType.GET_ORDER_LIST_SUCCESS;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
+
+    private final OrderService orderService;
+
+    public ApiResponse<List<OrderResponse>> getOrderList() {
+        return ApiResponse.success(GET_ORDER_LIST_SUCCESS, orderService.getOrders());
+    }
 }
