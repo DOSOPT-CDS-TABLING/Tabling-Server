7396d4e6b6b8fab00f6419af080c7e449da85dfd
transfer_kk <kseysh123@naver.com>
2023-11-21 14:29:12 +0900

    feat: #8 OrderController에서 getOrderDetail Url 연결

--- src/main/java/org/sopt/tablingServer/order/presentation/OrderController.java
+++ src/main/java/org/sopt/tablingServer/order/presentation/OrderController.java
@@ -5,11 +5,14 @@ import lombok.RequiredArgsConstructor;
 
 import org.sopt.tablingServer.common.dto.ApiResponse;
 import org.sopt.tablingServer.order.application.OrderService;
+import org.sopt.tablingServer.order.dto.response.OrderDetailResponse;
 import org.sopt.tablingServer.order.dto.response.OrderListResponse;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static org.sopt.tablingServer.common.exception.model.SuccessType.GET_ORDER_DETAIL_SUCCESS;
 import static org.sopt.tablingServer.common.exception.model.SuccessType.GET_ORDER_LIST_SUCCESS;
 
 @RestController
@@ -22,5 +25,10 @@ public class OrderController {
     @GetMapping
     public ApiResponse<List<OrderListResponse>> getOrderList() {
         return ApiResponse.success(GET_ORDER_LIST_SUCCESS, orderService.getOrders());
+    }
+
+    @GetMapping("/{orderId}")
+    public ApiResponse<OrderDetailResponse> getOrderDetail(@PathVariable Long orderId) {
+        return ApiResponse.success(GET_ORDER_DETAIL_SUCCESS, orderService.getOrderByOrderId(orderId));
     }
 }
