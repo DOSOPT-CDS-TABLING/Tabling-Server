441108d8ace43cd46f11204eaaa992a8a369af0a
ddongseop <dlehdtjq00o@gmail.com>
2023-11-22 02:46:04 +0900

    Merge remote-tracking branch 'origin/develop' into feat/#6-make_reserve_api

---
+++ src/main/java/org/sopt/tablingServer/order/controller/OrderController.java
@@ -0,0 +1,39 @@
+package org.sopt.tablingServer.order.controller;
+
+import org.sopt.tablingServer.common.dto.ApiResponse;
+import org.sopt.tablingServer.order.dto.request.OrderReserveRequest;
+import org.sopt.tablingServer.order.dto.response.OrderReserveResponse;
+import org.sopt.tablingServer.order.dto.response.OrderDetailResponse;
+import org.sopt.tablingServer.order.dto.response.OrderListResponse;
+
+import java.util.List;
+
+import lombok.RequiredArgsConstructor;
+
+import org.sopt.tablingServer.order.service.OrderService;
+import org.springframework.web.bind.annotation.*;
+
+import static org.sopt.tablingServer.common.exception.model.SuccessType.*;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @GetMapping
+    public ApiResponse<List<OrderListResponse>> orderList() {
+        return ApiResponse.success(GET_ORDER_LIST_SUCCESS, orderService.findOrderList());
+    }
+
+    @GetMapping("/{orderId}")
+    public ApiResponse<OrderDetailResponse> orderDetail(@PathVariable Long orderId) {
+        return ApiResponse.success(GET_ORDER_DETAIL_SUCCESS, orderService.findOrder(orderId));
+    }
+
+    @PostMapping("/reserve")
+    public ApiResponse<OrderReserveResponse> orderReserve(@RequestBody OrderReserveRequest request) {
+        return ApiResponse.success(RESERVE_ORDER_SUCCESS, orderService.createOrder(request));
+    }
+}
