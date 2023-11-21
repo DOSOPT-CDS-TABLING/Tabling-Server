a8d1c60a416ecb22bc2292601e0f94b10298600d
transfer_kk <kseysh123@naver.com>
2023-11-21 14:06:49 +0900

    feat: #8 OrderDetailResponse 구현

--- src/main/java/org/sopt/tablingServer/order/dto/response/OrderDetailResponse.java
+++ src/main/java/org/sopt/tablingServer/order/dto/response/OrderDetailResponse.java
@@ -1,4 +1,31 @@
 package org.sopt.tablingServer.order.dto.response;
 
-public class OrderDetailResponse {
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.time.LocalDateTime;
+import org.sopt.tablingServer.order.domain.Order;
+
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public record OrderDetailResponse(
+        Long orderId,
+        String shopName,
+        int waitingNumber,
+        int beforeCount,
+        LocalDateTime orderDate,
+        int personCount,
+        String orderStatus,
+        int totalPrice
+) {
+    public static OrderDetailResponse of(Order order) {
+        return new OrderDetailResponse(
+                order.getId(),
+                order.getShopName(),
+                order.getWaitingNumber(),
+                order.getBeforeCount(),
+                order.getOrderDate(),
+                order.getPersonCount(),
+                order.getOrderStatus().getValue(),
+                order.getTotalPrice()
+        );
+    }
 }
